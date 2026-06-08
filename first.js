2769db240076674ca195dcb2025fca0a5e572b35
sudhanshu015 <sudhanshukumar1139@gmail.com>
2026-06-08 23:05:10 +0530

    upadate 1.1 date 08/06/26

---
+++ first.js
@@ -0,0 +1,10 @@
+console.log("hellow ");
+a=10;
+console.log(a);
+a=20;
+console.log(a);
+a="sudhanshu"
+console.log(a);
+console.log("babu","avnish","sudhanshu");
+age=24;
+console.log("My age is ",age)
---
+++ varLetConst.js
@@ -0,0 +1,8 @@
+var a=5;
+console.log(a);
+var a=6;
+console.log(a)
+const b=5;
+console.log(b);
+const b=7;
+console.log(b);
